d9da4a97827b33184a41e68451bfee3d670b78f8
Chengruidong Zhang <chengzhang@microsoft.com>
2023-03-07 19:43:39 +0800

    batch profile scripts

--- batch_profile.py
+++ batch_profile.py
@@ -1,15 +1,14 @@
 import os
+import sys
 import math
 import datetime
-import subprocess
+from typing import Dict, Any
 
 import pandas as pd
 
 from onnx_conv import test_op
 
 
-GPU_ID = 1  # 1 2 0 1 2 0
-ALGO = 4    # 0 1 2 4 6 5
 ALGO_ENUM = [
     'CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_GEMM',
     'CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_PRECOMP_GEMM',
@@ -20,70 +19,76 @@ ALGO_ENUM = [
     'CUDNN_CONVOLUTION_FWD_ALGO_WINOGRAD',
     'CUDNN_CONVOLUTION_FWD_ALGO_WINOGRAD_NONFUSED',
 ]
-MODE = 'energy'
-
-if MODE == 'energy':
-    CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'search_space', 'regnet_convs_unique.csv')
-    # CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'search_space', 'regnet_convs_expand.csv')
-elif MODE == 'latency':
-    CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'search_space', 'regnet_convs_expand.csv')
-else:
-    raise ValueError(f'mode not supported: {MODE}')
-
-LOG_FOLDER = os.path.join(os.path.dirname(__file__), 'logs', MODE)
-if not os.path.exists(LOG_FOLDER):
-    os.makedirs(LOG_FOLDER)
-LOG_PATH = os.path.join(LOG_FOLDER, f'{ALGO}-{datetime.date.today().strftime("%Y%m%d")}.csv')
-
 FEATURES = ['hw', 'in_channels', 'out_channels', 'kernel_size', 'stride', 'groups']
 
 
-def run_cmd(cmd):
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = p.communicate()
-    stdout = stdout.decode('utf-8')
-    stderr = stderr.decode('utf-8')
-    return stdout, stderr
+class BatchProfiler(object):
 
+    def __init__(self, mode: str = 'energy', gpu_id: int = 0, algo: int = 0):
+        self._mode = mode
+        self._algo = algo
+        self._gpu_id = gpu_id
+        config_set = {'energy': 'unique', 'latency': 'expand'}[mode]
+        self._config_path = os.path.join(
+            os.path.dirname(__file__),
+            'search_space',
+            f'regnet_convs_{config_set}.csv',
+        )
+        log_folder = os.path.join(os.path.dirname(__file__), 'logs', mode)
+        if not os.path.exists(log_folder):
+            os.makedirs(log_folder)
+        self._log_path = os.path.join(
+            log_folder,
+            f'{algo}-{datetime.date.today().strftime("%Y%m%d")}.csv',
+        )
+        self._profile = {
+            'energy': self._profile_energy,
+            'latency': self._profile_latency,
+        }[self._mode]
 
-def profile_energy(config):
-    config['gpu_id'] = GPU_ID
-    config['algo'] = ALGO
-    config['profile_energy'] = 1
-    config['num_warmups'] = 10
-    config['num_iters'] = 100
-    trial_results = test_op(config)
-    if trial_results is None:
-        return None
-    config['num_iters'] = math.ceil(10 / trial_results['latency'])
-    config['num_warmups'] = 200
-    return test_op(config)
+    def _profile_config(self, num_warmups: int, num_iters: int, config: Dict[str, Any]):
+        return test_op({
+            'gpu_id': self._gpu_id,
+            'algo': self._algo,
+            'num_warmups': num_warmups,
+            'num_iters': num_iters,
+            **config
+        })
 
+    def _profile_energy(self, config):
+        trial_results = self._profile_config(10, 100, config)
+        if trial_results is None:
+            return None
+        num_iters = math.ceil(10 / trial_results['latency'])
+        return self._profile_config(200, num_iters, config)
 
-def profile_latency(config):
-    config['gpu_id'] = GPU_ID
-    config['algo'] = ALGO
-    config['profile_energy'] = 0
-    config['num_warmups'] = 200
-    config['num_iters'] = 1000
-    return test_op(config)
+    def _profile_latency(self, config):
+        return self._profile_config(200, 1000, config)
+
+    def run(self):
+        first_record = True
+        for i, row in pd.read_csv(self._config_path).iterrows():
+            config = row.to_dict()
+            print(f'#{i}: {config}')
+            results = self._profile(config)
+            if results is None:
+                continue
+            data = config | results
+            if first_record:
+                with open(self._log_path, 'w') as f:
+                    f.write(','.join(data.keys()) + '\n')
+                first_record = False
+            with open(self._log_path, 'a') as f:
+                f.write(','.join([str(x) for x in data.values()]) + '\n')
 
 
 if __name__ == '__main__':
-    first_record = True
-    for i, row in pd.read_csv(CONFIG_PATH).iterrows():
-        config = row.to_dict()
-        print(f'#{i}: {config}')
-        results = {
-            'energy': profile_energy,
-            'latency': profile_latency,
-        }[MODE](config)
-        if results is None:
-            continue
-        data = config | results
-        if first_record:
-            with open(LOG_PATH, 'w') as f:
-                f.write(','.join(data.keys()) + '\n')
-            first_record = False
-        with open(LOG_PATH, 'a') as f:
-            f.write(','.join([str(x) for x in data.values()]) + '\n')
+    gpu_id = 0
+    algo = 0
+    for arg in sys.argv[1:]:
+        k, v = arg[2:].split('=')
+        if k.lower().startswith('gpu'):
+            gpu_id = int(v)
+        elif k.lower().startswith('algo'):
+            algo = int(v)
+    BatchProfiler('energy', gpu_id, algo).run()
